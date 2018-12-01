c6a5349057b30ac57574499f0019f942aab5e173
Matt Basile <matthewbasile2@gmail.com>
2018-12-01 18:10:16 -0500

    Added DOM variables for Villain DOM Objects.

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ const arena = document.querySelector(".arena");
 console.log(arena);
 //================HERO COLUMN==================
 const heroCol = document.querySelector(".hero"); 
-console.log(heroCol);
 const heroSelect = document.querySelector(".hero-roster")
-console.log(heroSelect);
-const heroTitle = document.querySelector(".hero-title")
+const heroName = document.querySelector(".hero-title")
 const heroHealth = document.querySelector(".hero-health")
-console.log(heroTitle);
-console.log(heroHealth);
 const heroAttack = document.querySelector(".hero-attack")
 const heroSaiyan = document.querySelector(".hero-saiyan")
 const heroHeal = document.querySelector(".hero-heal")
-console.log(heroAttack);
-console.log(heroSaiyan);
-console.log(heroHeal);
+
+//================VILLAIN COLUMN==================
+const vilCol = document.querySelector(".villain"); 
+const vilSelect = document.querySelector(".vil-roster")
+const vilName = document.querySelector(".vil-title")
+const vilHealth = document.querySelector(".vil-health")
+const vilAttack = document.querySelector(".vil-attack")
+const vilSaiyan = document.querySelector(".vil-saiyan")
+const vilHeal = document.querySelector(".vil-heal")
+
+console.log(vilCol)
+console.log(vilSelect)
+console.log(vilName)
+console.log(vilHealth)
+console.log(vilAttack)
+console.log(vilSaiyan)
+console.log(vilHeal)
