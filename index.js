6a1117c87d4cd642f534347a986e512635f2ee53
Matt Basile <matthewbasile2@gmail.com>
2018-12-01 18:04:20 -0500

    Added DOM variables for Hero DOM Objects.

---
+++ index.js
@@ -0,0 +1,19 @@
+//DOM Objects we'll need to manipulate.
+//=================ARENA=====================
+const arena = document.querySelector(".arena"); 
+console.log(arena);
+//================HERO COLUMN==================
+const heroCol = document.querySelector(".hero"); 
+console.log(heroCol);
+const heroSelect = document.querySelector(".hero-roster")
+console.log(heroSelect);
+const heroTitle = document.querySelector(".hero-title")
+const heroHealth = document.querySelector(".hero-health")
+console.log(heroTitle);
+console.log(heroHealth);
+const heroAttack = document.querySelector(".hero-attack")
+const heroSaiyan = document.querySelector(".hero-saiyan")
+const heroHeal = document.querySelector(".hero-heal")
+console.log(heroAttack);
+console.log(heroSaiyan);
+console.log(heroHeal);
