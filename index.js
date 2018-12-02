7b6c0adde11e9547f5efacf96868d886b93c01dd
Matt Basile <matthewbasile2@gmail.com>
2018-12-01 19:40:22 -0500

    Created constructor functions, added Villains as well. LINES 25-102.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ console.log(arena);
 //================HERO COLUMN==================
 const heroCol = document.querySelector(".hero"); 
 const heroSelect = document.querySelector(".hero-roster")
+const heroSelectBtns = document.querySelectorAll(".hero-roster button")
+console.log(heroSelectBtns);
 const heroName = document.querySelector(".hero-title")
 const heroHealth = document.querySelector(".hero-health")
 const heroAttack = document.querySelector(".hero-attack")
@@ -20,34 +22,87 @@ const vilAttack = document.querySelector(".vil-attack")
 const vilSaiyan = document.querySelector(".vil-saiyan")
 const vilHeal = document.querySelector(".vil-heal")
 
+
+
+class Player {
+    constructor(attributes){
+        this.name = attributes.name;
+        this.health = attributes.name;
+        this.saiyanPwr = attributes.saiyanPwr;
+        this.attackPwr = attributes.saiyanPwr;
+        this.image = attributes.image;
+    }
+}
+
+//HEROS
+const goku = new Player({
+    name: "Goku",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/goku.png"
+});
+const masterR = new Player({
+    name: "Master Roshi",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/roshi.png"
+});
+const piccolo = new Player({
+    name: "Piccolo",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/piccolo.png"
+});
+const krillin = new Player({
+    name: "Krillin",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/krillin.png"
+});
 const heroes = {
-    "Goku":{
-        name: "Goku",
-        power: 10,
-        health: 50,
-        saiyan: 50,
-    },
-    "Master Roshi":{
-        name: "Master Roshi",
-        power: 10,
-        health: 60,
-        saiyan: 50,
-    },
-    "Piccolo": {
-        name: "Piccolo",
-        power: 10,
-        health: 70,
-        saiyan: 50,
-    },
-    "Krillin": {
-        name: "Krillin",
-        power: 10,
-        health: 80,
-        saiyan: 50,
-    },
-
+    goku,
+    masterR,
+    piccolo,
+    krillin
 }
-
-function selectHero(){
-
+console.log(heroes);
+//Villains
+const frieza = new Player({
+    name: "Frieza",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/frieza.png"
+});
+const kidBu = new Player({
+    name: "Kid Bu",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/kidbu.png"
+});
+const beerus = new Player({
+    name: "Beerus",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/beerus.png"
+});
+const cell = new Player({
+    name: "Cell",
+    health: 50,
+    saiyanPwr: 20,
+    attackPwr: 10,
+    image: "img/cell.png"
+});
+const villains = {
+    frieza,
+    kidBu,
+    beerus,
+    cell
 }
+console.log(villains);
